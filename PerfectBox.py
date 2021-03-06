59b5ecbaf4ac99c0413111d59cf90c92b7ddb797
JustBruh <s.platonov4@gmail.com>
2021-03-06 13:30:55 +0300

    Added getCurrentSpeed and syncWheels

--- PerfectBox.py
+++ PerfectBox.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as GPIO
 from i2clibraries import i2c_hmc5883l
+from wiringpi import pcf8591
 
 ##IN1 = 19   
 ##IN2 = 13
@@ -33,7 +34,7 @@ class controller():
         self.targetDirection
         self.targetSpeed
         self.targetDistance
-        self.syncOfWheels = {"lSpeed":, "rSpeed":}
+        self.speedOfWheels = {"lSpeed":, "rSpeed":}
 
     def countRoute(self, pathToMap):
         return self.targetDirection, self.targetDistance
@@ -41,9 +42,6 @@ class controller():
 
     def _setup_GPIO(self, IN1, IN2, IN3, IN4, ENA, ENB, SCL, SDA, ENCODERL, ENCODERR):
         
-        self.hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
-        self.hmc5883l.setContinuousMode()
-
         GPIO.cleanup()
         GPIO.setmode(GPIO.BCM)  
     
@@ -55,12 +53,19 @@ class controller():
         GPIO.setup(ENB, OUT)
         GPIO.setup(ENCODERL, IN)
         GPIO.setup(ENCODERR, IN)
-        self.l_Motor_Pwm = GPIO.PWM(ENA, FREQUENCY)
-        self.r_Motor_Pwm = GPIO.PWM(ENB, FREQUENCY)
 
-    def getCurrentDirection(self):
-        aX = self.hmc5883l.getHeading()
+    def getCurrentDirection(self, hmc5883l):
+        aX = hmc5883l.getHeading()
             return aX[0]
+
+    def getCurrentSpeed(self, PCF8591):
+        self.speedOfWheels=PCF8591.analogRead()
+        if self.speedOfWheels[1]==self.speedOfWheels[2]
+            self.isWheelsSyncing = True
+        else:
+            self.isWheelsSyncing = False
+        return self.speedOfWheels
+			
 
     def isRouteMatching(self, currentDirection, syncOfWheels, currentDistance):
 
@@ -70,7 +75,7 @@ class controller():
         else:
             self.isDistanceMatching = False
 
-        speedDiff = abs(syncOfWheels("lSpeed") - syncOfWheels("rSpeed") 
+        speedDiff = abs(speedOfWheels("lSpeed") - speedOfWheels("rSpeed") 
         if speedDiff == 0:
             self.isWheelsSyncing = True
         else:
@@ -83,9 +88,9 @@ class controller():
             self.isDirectionMatching = False
         return (self.isDirectionMatching, self.isWheelsSyncing, self.isDistanceMatching)
 
-    def startMotion(self, DUTY_CYCLE_L, DUTY_CYCLE_R):
-        self.l_Motor_Pwm.start(DUTY_CYCLE_L)
-        self.r_Motor_Pwm.start(DUTY_CYCLE_R)
+    def startMotion(self, DUTY_CYCLE_L, DUTY_CYCLE_R, l_Motor_Pwm, r_Motor_Pwm):
+        l_Motor_Pwm.start(DUTY_CYCLE_L)
+        r_Motor_Pwm.start(DUTY_CYCLE_R)
 
     def unloading(self):
         GPIO.output(hydrocylinder, HIGH)
@@ -101,9 +106,26 @@ class controller():
             directionDiff = int(self.getCurrentDirection())-self.targetDirection
             temp+=1
             if diff > 0:
-                self.l_Motor_Pwm.ChangeDutyCycle(self.DUTY_CYCLE_L+temp)
+                self.l_Motor_Pwm.ChangeDutyCycle(DUTY_CYCLE_L+temp)
             if diff < 0:
-                self.r_Motor_Pwm.ChangeDutyCycle(self.DUTY_CYCLE_R+temp)
+                self.r_Motor_Pwm.ChangeDutyCycle(DUTY_CYCLE_R+temp)
 
+    def synchWheels(self, l_Motor_Pwm, r_Motor_Pwm):
+        if self.isWheelsSyncing == False:
+            while self.isWheelsSyncing != True:
+                currentSpeed = self.getCurrentSpeed()
+                if currentSpeed[1]-currentSpeed[2]> 0:
+                    r_Motor_Pwm.ChangeDutyCycle(DUTY_CYCLE_R+temp)
+                if currentSpeed[1]-currentSpeed[2]< 0:
+                    l_Motor_Pwm.ChangeDutyCycle(DUTY_CYCLE_L+temp)
+            return True
+        else:
+            return "Wheels already synchronized"
 
-        
+hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
+hmc5883l.setContinuousMode()
+
+pcf8591 = PCF8591(0x48)
+
+l_Motor_Pwm = GPIO.PWM(ENA, FREQUENCY)
+r_Motor_Pwm = GPIO.PWM(ENB, FREQUENCY)
